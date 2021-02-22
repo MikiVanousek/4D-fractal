3f5f255342ec9929d60ef1fd007a99a58af7188c
Mikuláš Vanoušek <vanousekmikulas@gmail.com>
2021-02-22 22:08:22 +0100

    Fragment shader now runs for every pixel in the window.

--- fractal-cpp/src/fr.shader
+++ fractal-cpp/src/fr.shader
@@ -1,7 +1,7 @@
-#version 400
-
+#version 330 core
 out vec4 color;
 
-void main{
-	color = vec4(0.5, 0.5, 1.0, 1.0);
-}
+void main()
+{
+    color = vec4(1.0, 0.0, 0.0, 1.0);
+};
--- fractal-cpp/src/main.cpp
+++ fractal-cpp/src/main.cpp
@@ -2,42 +2,101 @@
 #include <stdint.h>
 #include <stdio.h>
 
+#include <string>
+#include <iostream>
+#include <fstream>
+
 #include <GL/glew.h>
 #define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
 #include <linmath.h>
 
 
-static void error_callback(int error, const char* description)
-{
+static void ErrorCallback(int error, const char* description) {
     fprintf(stderr, "Error: %s\n", description);
 }
 
-static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
-{
+static void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods) {
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
         glfwSetWindowShouldClose(window, GLFW_TRUE);
+    printf("Key pressed: %d\n", key);
 }
 
-static const char* vertex_shader_text =
-"#version 110\n"
-"uniform mat4 MVP;\n"
-"attribute vec3 vCol;\n"
-"attribute vec2 vPos;\n"
-"varying vec3 color;\n"
-"void main()\n"
-"{\n"
-"    gl_Position = MVP * vec4(vPos, 0.0, 1.0);\n"
-"    color = vCol;\n"
-"}\n";
 
-static const char* fragment_shader_text =
-"#version 110\n"
-"varying vec3 color;\n"
-"void main()\n"
-"{\n"
-"    gl_FragColor = vec4(color, 1.0);\n"
-"}\n";
+std::string ReadFile(const char* filePath) {
+    std::string content;
+    std::ifstream fileStream(filePath, std::ios::in);
+
+    if (!fileStream.is_open()) {
+        std::cerr << "Could not read file " << filePath << ". File does not exist." << std::endl;
+        return "";
+    }
+
+    std::string line = "";
+    while (!fileStream.eof()) {
+        std::getline(fileStream, line);
+        content.append(line + "\n");
+    }
+
+    fileStream.close();
+    return content;
+}
+
+static unsigned int CompileShader(const std::string& source, unsigned int type) {
+    char* shaderTypeName;
+    if (type == GL_VERTEX_SHADER)
+        shaderTypeName = (char*)"Vertex Shader";
+    else
+        shaderTypeName = (char*)"Fragment Shader";
+    
+
+    unsigned int id = glCreateShader(type);
+    const char* src = source.c_str();
+    glShaderSource(id, 1, &src, nullptr);
+    glCompileShader(id);
+    
+    int result;
+    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+    if (result == GL_FALSE) {
+        int length;
+        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+        char* message = (char*)alloca(length * sizeof(char));
+        glGetShaderInfoLog(id, length, &length, message);
+
+        printf("%s attaching failed!\n", shaderTypeName);
+        std::cout << message << std::endl;
+
+        glDeleteShader(id);
+
+    }
+    else {
+        printf("%s succsessfully attached!\n", shaderTypeName);
+    }
+    return id;
+}
+
+static int AttachShaders() {
+    unsigned int program = glCreateProgram();
+
+    std::string vertShaderStr = ReadFile("src/vs.shader");
+    std::string fragShaderStr = ReadFile("src/fr.shader");
+
+    
+    unsigned int vs = CompileShader(vertShaderStr, GL_VERTEX_SHADER);
+    unsigned int fs = CompileShader(fragShaderStr, GL_FRAGMENT_SHADER);
+ 
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    
+    glLinkProgram(program);
+    glValidateProgram(program);
+    
+    /* Removes shaders from cash. */
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    return program;
+}
 
 int main(void)
 {
@@ -52,17 +111,8 @@ int main(void)
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
 
     /* Seting up callbacks */
-    glfwSetErrorCallback(error_callback);
-    glfwSetKeyCallback(window, key_callback);
-
-    /* Getting version info */
-    const GLubyte* renderer = glGetString(GL_RENDERER); // get renderer string
-    const GLubyte* version = glGetString(GL_VERSION); // version as a string
-    printf("Renderer: %s\n", renderer);
-    printf("OpenGL version supported %s\n", version);
-
-    
-    glfwSwapInterval(1);
+    glfwSetErrorCallback(ErrorCallback);
+    glfwSetKeyCallback(window, KeyCallback);
 
   
     if (!window)
@@ -80,11 +130,27 @@ int main(void)
         return -1;
     }
 
+    /* Getting version info */
+    const GLubyte* renderer = glGetString(GL_RENDERER); // get renderer string
+    const GLubyte* version = glGetString(GL_VERSION); // version as a string
+    printf("Renderer: %s\n", renderer);
+    printf("OpenGL version supported %s\n", version);
+
+
+    glfwSwapInterval(1);
+    unsigned int shader = AttachShaders();
+    glUseProgram(shader);
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
+
+        //glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        /* Drawing the whole screen using the shader */
+        glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
@@ -96,3 +162,23 @@ int main(void)
     glfwTerminate();
     return 0;
 }
+
+
+/* This code draws a triangle with openGl. It is now obsolete, but I keep it here in case I ever need it.
+float positions[6] = {
+    0.0f, -1.0f,
+
+    1.0f, 0.0f,
+    1.0f, 0.5f
+};
+
+
+unsigned int buffer;
+glGenBuffers(1, &buffer);
+glBindBuffer(GL_ARRAY_BUFFER, buffer);
+glBufferData(GL_ARRAY_BUFFER, sizeof(positions), positions, GL_STATIC_DRAW);
+
+glEnableVertexAttribArray(0);
+
+glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float)*2, (const void*)8);
+*/
