84ac9d1a357b9159b047094738565ad38556f1d1
Mikuláš Vanoušek <vanousekmikulas@gmail.com>
2021-02-22 18:22:07 +0100

    Added gl functinos with glew

--- fractal-cpp/src/main.cpp
+++ fractal-cpp/src/main.cpp
@@ -1,24 +1,84 @@
+#include <stdlib.h>
 #include <stdint.h>
+#include <stdio.h>
+
+#include <GL/glew.h>
+#define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
+#include <linmath.h>
+
+
+static void error_callback(int error, const char* description)
+{
+    fprintf(stderr, "Error: %s\n", description);
+}
+
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+}
+
+static const char* vertex_shader_text =
+"#version 110\n"
+"uniform mat4 MVP;\n"
+"attribute vec3 vCol;\n"
+"attribute vec2 vPos;\n"
+"varying vec3 color;\n"
+"void main()\n"
+"{\n"
+"    gl_Position = MVP * vec4(vPos, 0.0, 1.0);\n"
+"    color = vCol;\n"
+"}\n";
+
+static const char* fragment_shader_text =
+"#version 110\n"
+"varying vec3 color;\n"
+"void main()\n"
+"{\n"
+"    gl_FragColor = vec4(color, 1.0);\n"
+"}\n";
 
 int main(void)
 {
     GLFWwindow* window;
 
     /* Initialize the library */
-    if (!glfwInit())
+    if (!glfwInit()) {
         return -1;
-
+    }
+   
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+
+    /* Seting up callbacks */
+    glfwSetErrorCallback(error_callback);
+    glfwSetKeyCallback(window, key_callback);
+
+    /* Getting version info */
+    const GLubyte* renderer = glGetString(GL_RENDERER); // get renderer string
+    const GLubyte* version = glGetString(GL_VERSION); // version as a string
+    printf("Renderer: %s\n", renderer);
+    printf("OpenGL version supported %s\n", version);
+
+    
+    glfwSwapInterval(1);
+
+  
     if (!window)
     {
+        fprintf(stderr, "ERROR: Could not open the window!");
         glfwTerminate();
         return -1;
     }
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
+
+    /* Initialze Glew */
+    if (glewInit() != GLEW_OK) {
+        return -1;
+    }
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
